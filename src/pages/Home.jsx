22d380ea0cd1279b41881d31b3c172142b72aeaa
enriquecr01 <30837445+enriquecr01@users.noreply.github.com>
2023-10-16 13:16:38 -0700

    added react router and main starry background

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import "../styles/home.css"
+
+export default function Home() {
+  return (
+    <div>
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+      <div id="title">
+        <span>Star Near on Earth</span>
+        <br></br>
+        <span>Check the nearest stars on Earth</span>
+      </div>
+    </div>
+  );
+}
