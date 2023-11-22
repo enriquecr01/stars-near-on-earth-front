0200bbed3d7a22e8c8bfe949ed3a624e932d0437
enriquecr01 <30837445+enriquecr01@users.noreply.github.com>
2023-11-21 21:01:55 -0800

    feat: :iphone: Improve the responsive HOME and added description
    Improve the responsive HOME and added description of the behaviour of the buttons

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -11,8 +11,17 @@ export default function Home() {
 
   return (
     <div>
-      <div id="title">
+      <div className="main-title">
         <h1>STARS NEAR EARTH</h1>
+
+        <p className="description-paragraph">
+          This webpage was made with the purpose of practice the technique named web scrapping. <br />
+          
+          I use the table of <a className="link-class" target="_blank" href="https://en.wikipedia.org/wiki/List_of_nearest_stars_and_brown_dwarfs">this page</a> of Wikipedia. <br />
+          
+          The button of Pre Saved stars load faster the stars that is because doesn't use the technique of web scraping. <br />
+          If you want to view how it's work the scrapper. Use the button of Scrap Stars.
+        </p>
 
         <div className="container">
             <Link to={"/pre-saved"} className="btn">Pre Saved Stars</Link>
