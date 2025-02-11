7a7b5db83ca856ecc504fc8a4d7855e601261ab7
Enrique Chavez R. <30837445+enriquecr01@users.noreply.github.com>
2025-02-10 17:05:38 -0800

    docs: :speech_balloon: update description of stars near earth

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,13 +1,12 @@
-import { useEffect } from "react"
-import "../styles/home.css"
-import "../styles/main.css"
-import { Link } from "react-router-dom"
+import { useEffect } from "react";
+import "../styles/home.css";
+import "../styles/main.css";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-
   useEffect(() => {
-    document.title = "Stars Near Earth"
-  })
+    document.title = "Stars Near Earth";
+  });
 
   return (
     <div>
@@ -15,17 +14,38 @@ export default function Home() {
         <h1>STARS NEAR EARTH</h1>
 
         <p className="description-paragraph">
-          This webpage was made with the purpose of practice the technique named web scrapping. <br />
-          
-          I use the table of <a className="link-class" target="_blank" href="https://en.wikipedia.org/wiki/List_of_nearest_stars_and_brown_dwarfs">this page</a> of Wikipedia. <br />
-          
-          The button of Pre Saved stars load faster the stars that is because doesn't use the technique of web scraping. <br />
-          If you want to view how it's work the scrapper. Use the button of Scrap Stars.
+          This webpage was made by{"\u00A0"}
+          <a
+            className="link-class"
+            target="_blank"
+            href="https://enriquechavezr.com/"
+          >
+            Enrique Chavez Romero
+          </a>
+          {"\u00A0"}he wanted to practice a technique named web scrapping.{" "}
+          <br />
+          He used the table of{"\u00A0"}
+          <a
+            className="link-class"
+            target="_blank"
+            href="https://en.wikipedia.org/wiki/List_of_nearest_stars_and_brown_dwarfs"
+          >
+            this page of Wikipedia
+          </a>
+          {"\u00A0"}to get all the data of nearest stars of earth then he scrap
+          all the information that he wants and ordered to present here. <br />
+          There are two buttons: The button of Pre Saved stars load faster is
+          because doesn't use the technique of web scraping. If you want to view
+          the scraped data from wikipedia then use the button of Scrap Stars.
         </p>
 
         <div className="container">
-            <Link to={"/pre-saved"} className="btn">Pre Saved Stars</Link>
-            <Link to={"/scrap-stars"} className="btn">Scrap Stars</Link>
+          <Link to={"/pre-saved"} className="btn">
+            Pre Saved Stars
+          </Link>
+          <Link to={"/scrap-stars"} className="btn">
+            Scrap Stars
+          </Link>
         </div>
       </div>
       <div id="stars"></div>
