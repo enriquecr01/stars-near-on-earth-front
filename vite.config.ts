df681dd136401b816f3be459487c8e4361d773e2
Enrique Chavez R. <30837445+enriquecr01@users.noreply.github.com>
2026-03-31 11:58:38 -0700

    build: :wrench: removed custom URL/basename /proyectos/stars-near-earth

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/proyectos/stars-near-earth',
+    base: '/',
     plugins: [react()]
 })
